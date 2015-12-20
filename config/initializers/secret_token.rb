d7eee740bd349ec393ff7b3557084188475d62e6
psyomn <lethaljellybean@gmail.com>
2015-12-19 23:52:18 -0500

    first commit of UCA in rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Uca::Application.config.secret_token = '9f456f35c18a86756c6f2867c259809f7e373550cd3bb114234b232d2f89bf9f9aa612f4ff3b9f71a201c6871b539918dfc717c5199cbb05b93d9faebe6100cf'

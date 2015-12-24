d5de34e54985552bf75e056fbf847d3deb79fcc7
psyomn <lethaljellybean@gmail.com>
2015-12-24 04:04:39 -0400

    controller: application, add cancan

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  rescue_from CanCan::AccessDenied do |exception|
+    Offense.create(
+      user_id: current_user.id,
+      offense_type: Offense::AuthOnResource,
+      attempted_resource: "#{exception.action} #{exception.subject}",
+      message: exception.message)
+
+    redirect_to main_app.root_url, :alert => exception.message
+  end
 end

54563f25ea088f4290f6f63a93c520df83485c51
psyomn <lethaljellybean@gmail.com>
2016-01-04 23:38:18 -0500

    models: ratings, add validators

--- app/models/rating.rb
+++ app/models/rating.rb
@@ -3,6 +3,10 @@ class Rating < ActiveRecord::Base
   belongs_to :song
   Max = 5
 
+  validates :score, inclusion: 1..5
+  validates :user_id, presence: true
+  validates :song_id, presence: true
+
   # attr_accessible :score
   private
     def rating_params

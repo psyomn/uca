fb6d8979afcddaec2513b4d12fde13b601e4d6a1
psyomn <lethaljellybean@gmail.com>
2016-02-14 20:55:26 -0500

    Require unique nicknames

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
+  before_filter :configure_permitted_parameters, if: :devise_controller?
+
   rescue_from CanCan::AccessDenied do |exception|
     Offense.create(
       user_id: current_user.try(:id),
@@ -9,5 +11,11 @@ class ApplicationController < ActionController::Base
       message: exception.message)
 
     redirect_to main_app.root_url, :alert => exception.message
+  end
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) << :nickname
   end
 end
